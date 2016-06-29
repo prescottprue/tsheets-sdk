99ab431d72871086a3c157c9628c4ac716ba5a81
prescottprue <sprue.dev@gmail.com>
2016-06-29 00:53:02 -0700

    Updates package file. Adds unit testing of requests through nock.

--- test/unit/reports.spec.js
+++ test/unit/reports.spec.js
@@ -1,4 +1,6 @@
-/* global describe, it, expect */
+/* global describe, it, beforeEach, expect, nock */
+import { apiUrl, defaultStartDate } from '../../src/config'
+import { today } from '../../src/utils'
 import { reports } from '../../src'
 
 describe('reports', () => {
@@ -6,18 +8,57 @@ describe('reports', () => {
     expect(reports).to.be.an.object
   })
   describe('getPayrollReport method', () => {
+    beforeEach(() =>
+      nock(`${apiUrl}/reports`)
+        .post('/payroll', { data: { 'start_date': defaultStartDate, 'end_date': today() } })
+        .reply(200, {
+          results: {},
+          'supplemental_data': {}
+        })
+    )
     it('exists', () => {
       expect(reports).to.respondTo('getPayrollReport')
     })
+    it('calls tsheets POST endpoint', () =>
+      reports
+        .getPayrollReport()
+        .should.eventually.exist
+    )
   })
   describe('getProjectReport method', () => {
+    beforeEach(() =>
+      nock(`${apiUrl}/reports`)
+        .post('/project', { data: { 'start_date': defaultStartDate, 'end_date': today() } })
+        .reply(200, {
+          results: {},
+          'supplemental_data': {}
+        })
+    )
     it('exists', () => {
       expect(reports).to.respondTo('getProjectReport')
     })
+    it('calls tsheets POST endpoint', () =>
+      reports
+        .getProjectReport()
+        .should.eventually.exist
+    )
   })
   describe('getCurrentTotalsReport method', () => {
+    beforeEach(() =>
+      nock(`${apiUrl}/reports`)
+        .post('/currentTotals', { data: { 'start_date': defaultStartDate, 'end_date': today() } })
+        .reply(200, {
+          results: {},
+          'supplemental_data': {}
+        })
+    )
     it('exists', () => {
       expect(reports).to.respondTo('getCurrentTotalsReport')
     })
+    it('calls tsheets POST endpoint', () =>
+      reports
+        .getCurrentTotalsReport()
+        .should.eventually.exist
+    )
   })
 })
