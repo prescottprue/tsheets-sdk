951673f9cb31e09b2e85125b8ef4841fe81f93d0
nmccready <nemtcan@gmail.com>
2018-04-03 16:45:43 -0400

    REMOVED LODASH

--- src/timesheets.js
+++ src/timesheets.js
@@ -1,11 +1,10 @@
 import cruder, { get } from './utils/cruder'
 import { timesheetsEndpoint as endpoint } from './config'
-import { isObject } from 'lodash'
 
 const methods = {
   // TODO: Give option auto population using supplemental data
   get: q => {
-    if (!q || !isObject(q)) throw new Error('Object with query parameters required to get timesheets')
+    if (!q || typeof q !== 'object') throw new Error('Object with query parameters required to get timesheets')
     if (!q.start_date && !q.end_date && !q.modified_since && !q.modified_before && !q.ids) {
       throw new Error(
         'Query object consisting of start_date, end_date, modified_since, modified_before, or ids required'
