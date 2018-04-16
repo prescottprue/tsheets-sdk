f7f596ea32cb8bd654f94af2f6b3ee2c001d9613
Scott Prue <scott@residebrokerage.com>
2018-04-16 11:32:15 -0700

    Test variable cleanup [ci skip]
    Co-Authored-By: nmccready <nemtcan@gmail.com>

--- test/setup.js
+++ test/setup.js
@@ -1,17 +1,9 @@
-var chai = global.chai = require('chai')
-var expect = global.expect = chai.expect
-var should = global.should = chai.should()
-var nock = global.nock = require('nock')
+const chai = global.chai = require('chai')
+const expect = global.expect = chai.expect
+const should = global.should = chai.should()
+const nock = global.nock = require('nock')
+const chaiAsPromised = require('chai-as-promised')
+
 process.env.TSHEETS_TOKEN = 'test'
-// var Promise = require('es6-promise').Promise
-// global.Promise = Promise
 
-var chaiAsPromised = require('chai-as-promised')
 chai.use(chaiAsPromised)
-
-// var jsdom = require('jsdom')
-// var doc = jsdom.jsdom('<html><body></body></html>')
-// var win = doc.defaultView
-// global.document = doc
-// global.window = win
-// global.navigator = win.navigator
